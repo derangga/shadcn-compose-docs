ea1f0258cbee0cd61721edb9bd246a92d4d67ec6
derangga <derangga1011@gmail.com>
2025-10-09 10:53:42 +0700

    fix asset not displayed properly

--- src/components/ui/markdown-components/Preview.vue
+++ src/components/ui/markdown-components/Preview.vue
@@ -3,36 +3,36 @@ import { cn } from "@/lib/utils";
 import { computed } from "vue";
 
 interface Props {
-  name: string;
-  variant: string;
-  assetExtension: ".webp" | ".gif";
-  align?: "center" | "start" | "end";
+    name: string;
+    variant: string;
+    assetExtension: ".webp" | ".gif";
+    align?: "center" | "start" | "end";
 }
 
 const props = withDefaults(defineProps<Props>(), {
-  align: "center",
-  assetExtension: ".webp",
+    align: "center",
+    assetExtension: ".webp",
 });
 
 const imagePath = computed(
-  () =>
-    `/src/assets/components/${props.name}/${props.variant}${props.assetExtension}`
+    () =>
+        `/assets/components/${props.name}/${props.variant}${props.assetExtension}`,
 );
 </script>
 
 <template>
-  <div
-    :class="
-      cn(
-        'preview flex min-h-[350px] w-full justify-center p-10 items-center border rounded-md my-[6.5px]',
-        {
-          'items-center': align === 'center',
-          'items-start': align === 'start',
-          'items-end': align === 'end',
-        }
-      )
-    "
-  >
-    <img :src="imagePath" class="w-72" />
-  </div>
+    <div
+        :class="
+            cn(
+                'preview flex min-h-[350px] w-full justify-center p-10 items-center border rounded-md my-[6.5px]',
+                {
+                    'items-center': align === 'center',
+                    'items-start': align === 'start',
+                    'items-end': align === 'end',
+                },
+            )
+        "
+    >
+        <img :src="imagePath" class="w-72" />
+    </div>
 </template>
