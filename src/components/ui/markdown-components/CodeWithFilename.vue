e63fdd789c942016c51cd536b199dc7476f9f98e
derangga <derangga1011@gmail.com>
2025-10-09 10:27:12 +0700

    update sidebar docs

---
+++ src/components/ui/markdown-components/CodeWithFilename.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+interface Props {
+  filename: string;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <div class="code-block-wrapper border relative bg-secondary rounded-sm">
+    <div class="flex items-center justify-between px-4 py-2">
+      <span class="text-sm font-medium capitalize">{{ filename }}</span>
+    </div>
+
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+@reference "../../../style.css";
+
+.code-block-wrapper {
+  margin: 1.5rem 0;
+  border-radius: 0.5rem;
+  overflow: hidden;
+}
+
+:deep(pre[class*="language-"]) {
+  margin: 0 !important;
+}
+</style>
